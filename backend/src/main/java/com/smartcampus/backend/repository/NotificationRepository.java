03e9b7f0ea170c3215c4e314b08853309a39d3ef
sha355 <shamaliroshan050@gmail.com>
2026-04-02 22:32:21 +0530

    add NotificationType enum, Notification entity and NotificationRepository

---
+++ backend/src/main/java/com/smartcampus/backend/repository/NotificationRepository.java
@@ -0,0 +1,14 @@
+package com.smartcampus.backend.repository;
+
+import com.smartcampus.backend.entity.Notification;
+import com.smartcampus.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+    List<Notification> findByUserAndIsReadFalseOrderByCreatedAtDesc(User user);
+    long countByUserAndIsReadFalse(User user);
+}
