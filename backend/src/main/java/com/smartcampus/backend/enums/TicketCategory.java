b650ea8fb703710d0164b39ff5f97c9b36189a19
Milan Hasitha <hasithamilan2@gmail.com>
2026-04-09 12:52:50 +0530

    feat: add TicketCategory enum for categorizing incident tickets

---
+++ backend/src/main/java/com/smartcampus/backend/enums/TicketCategory.java
@@ -0,0 +1,12 @@
+package com.smartcampus.backend.enums;
+
+public enum TicketCategory {
+    ELECTRICAL,
+    PLUMBING,
+    HVAC,
+    IT_NETWORK,
+    CLEANING,
+    SECURITY,
+    FURNITURE,
+    OTHER
+}
